dd4a4f21415388938fb590f42883966f9c37dec4
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-06 10:01:17 +0530

    Time: 21 ms (66.91%) | Memory: 79.8 MB (19.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,21 +3,15 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    let map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            let frq = map.get(nums[i]) + 1;
-            map.set(nums[i], frq);
-        }
-        else {
-            map.set(nums[i], 1);
-        }
-    }
 
-    for (let value of map.values()) {
-        if (value != 1) {
+    // BETTER APPROACH USING SET
+
+    let set = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
             return true;
         }
+        set.add(nums[i]);
     }
     return false;
 
