7ffd4184e1199403c2ee0ff09a822fced9f32fa5
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-06 11:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (96.33%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.js
+++ 191-number-of-1-bits/number-of-1-bits.js
@@ -3,14 +3,23 @@
  * @return {number}
  */
 var hammingWeight = function (n) {
-    let binary = n.toString(2);
-    // console.log(binary)
-    let count = 0;
+    // let binary = n.toString(2);
+    // let count = 0;
 
-    for (let i = 0; i < binary.length; i++) {
-        if (binary[i] == 1) {
-            count++;
+    // for (let i = 0; i < binary.length; i++) {
+    //     if (binary[i] == 1) {
+    //         count++;
+    //     }
+    // }
+    // return count;
+
+    let count = 0;
+    while (n > 0) {
+        if (n & 1) {   // if(n&1==1)
+            count++
         }
+        n = n >> 1;
+
     }
     return count;
 
