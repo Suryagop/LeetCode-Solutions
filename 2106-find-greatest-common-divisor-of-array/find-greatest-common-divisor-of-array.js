1fa6e05bdcc330da9886ddf9052e2d3df0dc0fd6
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-10 09:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (71.57%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
@@ -5,12 +5,20 @@
 var findGCD = function (nums) {
     let a = Math.min(...nums);
     let b = Math.max(...nums);
-    // console.log(a,b)
-    for (let i = a; i >= 1; i--) {
-        if (a % i == 0 && b % i == 0) {
-            return i;
-            break;
-        }
+
+    //BRUTE FORCE 
+
+    // for (let i = a; i >= 1; i--) {
+    //     if (a % i == 0 && b % i == 0) {
+    //         return i;
+    //         break;
+    //     }
+    // }
+
+    function gcd(a, b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
+    return gcd(a, b)
 
 };
