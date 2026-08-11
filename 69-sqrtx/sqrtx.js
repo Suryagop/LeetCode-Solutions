89622e2bf9fde52cdd82edaa4965dd7f45923483
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-11 21:00:28 +0530

    Time: 1 ms (75.65%) | Memory: 55.8 MB (54.20%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,10 +3,22 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-    let i = 1;
-    for (i = 1; i * i <= x; i++) {
-        if (i * i == x) return i;
+    // let i = 1;
+    // for (i = 1; i * i <= x; i++) {
+    //     if (i * i == x) return i;
+    // }
+    // return i - 1;
+
+    //Optimized( Binary Search )
+    let first = 1, last = x, ans = 0;
+    while (first <= last) {
+        let mid = Math.floor((first + last) / 2);
+        if (mid <= Math.floor(x / mid)) {            // if(mid * mid <= x){}
+            ans = mid;
+            first = mid + 1;
+        }
+        else last = mid - 1;
     }
-    return i - 1;
+    return ans;
 
 };
