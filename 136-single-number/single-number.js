eb6d1b30a991fcdfdecc2780a47f0e6bf917e1d1
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-12 10:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (75.75%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -2,22 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            let frq = map.get(nums[i]) + 1;
-            map.set(nums[i], frq);
-        }
-        else {
-            map.set(nums[i], 1)
-        }
-    }
 
-    for (let key of map.keys()) {
-        if (map.get(key) == 1) {
-            return key;
-        }
+var singleNumber = function (nums) {
+
+    // OPTIMIZED SOLUTION 
+    let ans = 0;
+    for (let i = 0; i < nums.length; i++) {
+        ans = ans ^ nums[i]
     }
+    return ans;
 
 };
