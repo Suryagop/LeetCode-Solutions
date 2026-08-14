a22da6a81f0713496e9424a57a3f96f79c015403
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-14 21:52:41 +0530

    Time: 4 ms (53.67%) | Memory: 56.4 MB (72.11%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let ans = nums[0], count = 1, i = 1;
+    while (i < nums.length) {
+        if (count == 0) {
+            ans = nums[i];
+            count = 1;
+
+        }
+        else {
+            if (nums[i] != ans) {
+                count = count - 1
+            }
+            else count++;
+
+        }
+        i++;
+    }
+    console.log(ans);
+    return ans;
+};
