84365a4fc439e6d7fbd8b24aaefa1e25b2621342
Surya Gopalakrishnan <133187905+Suryagop@users.noreply.github.com>
2026-08-20 19:42:12 +0530

    Time: 4 ms (16.20%) | Memory: 63 MB (53.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -6,7 +6,7 @@ var maxArea = function (height) {
     let maxarea = 0;
     let area = 0;
     let i = 0;
-    j = height.length - 1;
+    let j = height.length - 1;
     while (i <= j) {
         if (height[i] < height[j]) {
             area = (Math.abs(j - i)) * (height[i])
